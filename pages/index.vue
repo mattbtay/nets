9aa586c79a8ffed79316e43c556db4ae629f108c
Matt Taylor <mattbtay@gmail.com>
2021-04-22 13:10:31 -0500

    init

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+hello world
+  </div>
+</template>
+
+<script>
+const ical = require('ical')
+  export default {
+    data() {
+      events = {}
+    },
+     mounted(){
+       this.events = ical.parseICS('https://calendar.google.com/calendar/ical/8pv1frn7h2ml914el8cu7gb9a0%40group.calendar.google.com/public/basic.ics')
+    }  
+  }
+ 
+</script>
+
+<style lang="scss" scoped>
+
+</style>
