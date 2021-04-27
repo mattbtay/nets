8d5c1d2198928e8b7fab5f5911fc7933e112bccb
Matt Taylor <mattbtay@gmail.com>
2021-04-27 13:12:06 -0500

    UPDATE

--- netlify/functions/getEvents/getEvents.js
+++ netlify/functions/getEvents/getEvents.js
@@ -1,8 +1,14 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 
-const { google } = require("googleapis");
+const {
+  google
+} = require("googleapis");
 
-const { OAuth2 } = google.auth;
+const {
+  OAuth2
+} = google.auth;
+
+const fetch = require("node-fetch");
 
 const oAuth2Client = new OAuth2(
   "526989770589-2n746t5hf2r30b7taodum5t6tfmh337k.apps.googleusercontent.com",
@@ -11,44 +17,54 @@ const oAuth2Client = new OAuth2(
 
 
 oAuth2Client.setCredentials({
-  refresh_token:
-    "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
+  refresh_token: "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
 });
 
-const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
+const calendar = google.calendar({
+  version: "v3",
+  auth: oAuth2Client
+});
 
 
 
-const handler = function(event, context, callback) {
+const handler = function (event, context, callback) {
   var start = new Date();
   start.setHours(0, 0, 1, 0);
 
-  const tomorrow = new Date(start);;
-  tomorrow.setHours(23, 23, 23, 999);
-
-  calendar.events.list(
-    {
-      auth: oAuth2Client,
-      calendarId: "8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com",
-      singleEvents: false,
-      timeMin: start,
-      timeMax: tomorrow
-      // orderBy: 'startTime'
-    },
-    (error, response) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      callback(null, {
-        headers: {
-          "Access-Control-Allow-Origin": "*"
-        },
-        statusCode: 200,
-        body: JSON.stringify(response.data.items)
-      });
-    }
+  const tomorrow = new Date(
+    start
   );
+  tomorrow.setHours(
+    23,
+    23,
+    23,
+    999
+  );
+
+  const bodyparams = {
+    timeMin: start,
+    timeMax: tomorrow,
+  }
+
+  fetch(
+      `https://www.googleapis.com/calendar/v3/calendars/8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com/events?key=AIzaSyDam2ft4FISMtZ5i03yf9dCpRjpQHkfSe4` + new URLSearchParams({
+        timeMin: start,
+        timeMax: tomorrow,
+      }), {
+        method: 'GET',
+      }
+    )
+    .then(res => res.json())
+    .then(json => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(
+          json.items
+        )
+      });
+    });
 };
 
-module.exports = { handler };
+module.exports = {
+  handler
+};
