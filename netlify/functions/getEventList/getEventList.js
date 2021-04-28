2fe71183d0f02f946de692bbeaf150ca4fa929fe
Matt Taylor <mattbtay@gmail.com>
2021-04-28 16:00:05 -0500

    test

--- netlify/functions/getEventList/getEventList.js
+++ netlify/functions/getEventList/getEventList.js
@@ -1,22 +1,18 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const { google } = require("googleapis");
 
-// const { google } = require("googleapis");
+const { OAuth2 } = google.auth;
 
-// const { OAuth2 } = google.auth;
+const oAuth2Client = new OAuth2(
+  "526989770589-2n746t5hf2r30b7taodum5t6tfmh337k.apps.googleusercontent.com",
+  "5OnB48EwOV82XkrhEQNV-od9"
+);
 
-// const oAuth2Client = new OAuth2(
-//   "526989770589-2n746t5hf2r30b7taodum5t6tfmh337k.apps.googleusercontent.com",
-//   "5OnB48EwOV82XkrhEQNV-od9"
-// );
+oAuth2Client.setCredentials({
+  refresh_token:
+    "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
+});
 
-// const api = OAuth2;
-
-// oAuth2Client.setCredentials({
-//   refresh_token:
-//     "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
-// });
-
-// const calendar = google.calendar({ version: "v3", auth: api });
+const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
 
 const handler = function(event, context, callback) {
   var start = new Date();
@@ -26,37 +22,37 @@ const handler = function(event, context, callback) {
   end.setDate(end.getDate() + 1);
   end.setHours(00, 00, 00, 000);
 
-  // calendar.events.list(
-  //   {
-  //     auth: oAuth2Client,
-  //     calendarId: "8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com",
-  //     singleEvents: true,
-  //     timeMin: start,
-  //     timeMax: end
-  //     // orderBy: 'startTime'
-  //   },
-  //   (error, response) => {
-  //     if (error) {
-  //       console.log(error);
-  //       return;
-  //     }
-  //     callback(null, {
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*"
-  //       },
-  //       statusCode: 200,
-  //       body: JSON.stringify(response.data.items)
-  //     });
-  //   }
-  // );
+  calendar.events.list(
+    {
+      auth: oAuth2Client,
+      calendarId: "8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com",
+      singleEvents: true,
+      timeMin: start,
+      timeMax: end
+      // orderBy: 'startTime'
+    },
+    (error, response) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      // callback(null, {
+      //   headers: {
+      //     "Access-Control-Allow-Origin": "*"
+      //   },
+      //   statusCode: 200,
+      //   body: JSON.stringify(response.data.items)
+      // });
+    }
+  );
 
   callback(null, {
-          headers: {
-            "Access-Control-Allow-Origin": "*"
-          },
-          statusCode: 200,
-          body: JSON.stringify([])
-        });
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    statusCode: 200,
+    body: JSON.stringify([])
+  });
 };
 
 module.exports = { handler };
