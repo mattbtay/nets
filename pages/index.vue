49e9560cf9048b01bf5217676ad1a936c4034754
Matt Taylor <mattbtay@gmail.com>
2021-04-28 23:27:39 -0500

    update

--- pages/index.vue
+++ pages/index.vue
@@ -6,40 +6,44 @@
         <div class="pb-2">
         <h1 class="text-center pb-2 pt-5 px-5">Are there any nets today?</h1>
         <h1 v-if="eventStatus == 2" class="text-center pb-2 mainText" style="font-size:8rem">YES!</h1>
+        <h1 v-if="eventStatus == 0" class="text-center pb-2 mainText" style="font-size:8rem">...</h1>
         <h1 v-if="eventStatus == 1" class="text-center pb-2 mainText sad" style="font-size:8rem">Nope</h1>
         <!-- <p class="text-center pb-5">If you have a net you would like to submit, please let us know.</p> -->
         </div>
       </b-col>
     </b-row>
-    <b-row v-if="eventStatus == 0">
-      <b-col>
-        <h2 class="text-center text-primary">loading...</h2>
-      </b-col>
-    </b-row>
-    <b-row v-if="eventStatus == 2">
-      <b-table head-variant="dark" stacked="sm" responsive="true" striped :items="events" :fields="fields">
+    
+    <b-row>
+      <b-table head-variant="dark" stacked="sm" sort-by="start.dateTime" striped :items="events" :fields="fields" :busy="isBusy">
 
+        <template #table-busy>
+        <div class="text-center text-default my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
+        
         <template #cell(start)="data">
-          {{getDate(data.value.dateTime)}}
+         <div :class="[isPastTime(data.value.dateTime) ? 'past' : '']" v-html="getDate(data.value.dateTime)"></div>
         </template>
 
-        <template #cell(location)="data">
-          {{getDate(data.value)}}
+        <template #cell(freq)="data">
+          <div :class="[isPastTime(data.item.start.dateTime) ? 'past' : '']" v-html="data.item.summary"></div>
         </template>
 
         <!-- A virtual composite column -->
       <template #cell(loc)="data">
-        {{ makeColumn(formatContent(data.item.description), 0) }}
+        <div :class="[isPastTime(data.item.start.dateTime) ? 'past' : '']" v-html="makeColumn(formatContent(data.item.description), 0)"></div>
       </template>
 
       <!-- A virtual composite column -->
       <template #cell(desc)="data">
-        {{ makeColumn(formatContent(data.item.description), 1) }}
+        <div :class="[isPastTime(data.item.start.dateTime) ? 'past' : '']" v-html="makeColumn(formatContent(data.item.description), 1)"></div>
       </template>
 
       <!-- A virtual composite column -->
       <template #cell(org)="data">
-        <div v-html="makeColumn(formatContent(data.item.description), 2)" />
+        <div :class="[isPastTime(data.item.start.dateTime) ? 'past' : '']" v-html="makeColumn(formatContent(data.item.description), 2)" />
       </template>
 
 
@@ -47,20 +51,26 @@
             
       
     </b-row>
+    <b-row>
+      <b-col>
+        <p class="text-center">If you have a net you'd like to add, please reach out to <a href="http://qrz.com/db/ki5nda"  target="_blank">KI5NDA</a> on QRZ or on <a href="http://twitter.com/mattbtay" target="_blank">twitter</a></p>
+      </b-col>
+    </b-row>
   </b-container>
 
 </template>
 
 <script>
 import axios from 'axios'
-import {format, isPast} from 'date-fns'
+import {format, isPast, getTime} from 'date-fns'
   export default {
     data() {
       return {
+        isBusy: true,
         eventStatus: 0, // 0 = loading, 1 = no events, 2 = some events
         fields: [
           {key: 'start', label: 'Time'},
-          {key: 'summary', label: 'Name'},
+          {key: 'freq', label: 'Name'},
           {key: 'loc', label: 'Location'},
           {key: 'desc', label: 'Description'},
           {key: 'org', lable: 'Org.'}
@@ -78,6 +88,7 @@ import {format, isPast} from 'date-fns'
 			  }).then(res => {
           //debugger;
           this.events = res.data
+          this.isBusy = false;
           if(res.data.length > 0) {
             this.eventStatus = 2
           } else if (res.data.length = 0) {
@@ -102,14 +113,25 @@ import {format, isPast} from 'date-fns'
     },
     formatContent: function(content){
       // section breakdown FQ | Description | org | link to org
-      return content.split(" | ");
+      // debugger;
+      return content.split("|");
     },
     makeColumn: function(data, index){
       //debugger;
       return data[index]
     },
     isPastTime: function(date){
-      return isPast(new Date(date))
+      var event = new Date(date);
+      var today = new Date();
+      var eventHour = event.getHours();
+      var eventMin = event.getMinutes();
+      var todayHour = today.getHours();
+      var todayMin = today.getMinutes();
+      var todayTime = todayHour + (todayMin / 60);
+      var eventTime = eventHour + (eventMin / 60);
+      // debugger
+      // return isPast(new Date(date))
+      return eventTime < todayTime ? true : false
     }
   },
   
@@ -149,7 +171,9 @@ import {format, isPast} from 'date-fns'
   box-shadow:2px 2px 4px rgba(0,0,0,.5)
 }
 
-.disabled {
-  
+.past {
+  text-decoration:line-through;
+  text-decoration-color: red;
+  opacity:.3
 }
 </style>
