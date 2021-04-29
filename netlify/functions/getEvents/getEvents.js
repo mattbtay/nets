18273c33045d7dc4c4856f3ad26256d56c8f6516
Matt Taylor <mattbtay@gmail.com>
2021-04-29 10:47:16 -0500

    test again

--- netlify/functions/getEvents/getEvents.js
+++ netlify/functions/getEvents/getEvents.js
@@ -1,8 +1,12 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 
-const { google } = require("googleapis");
+const {
+  google
+} = require("googleapis");
 
-const { OAuth2 } = google.auth;
+const {
+  OAuth2
+} = google.auth;
 
 const oAuth2Client = new OAuth2(
   "526989770589-2n746t5hf2r30b7taodum5t6tfmh337k.apps.googleusercontent.com",
@@ -11,52 +15,29 @@ const oAuth2Client = new OAuth2(
 
 
 oAuth2Client.setCredentials({
-  refresh_token:
-    "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
+  refresh_token: "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
 });
 
-const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
+const calendar = google.calendar({
+  version: "v3",
+  auth: oAuth2Client
+});
 
+const {startOfToday, startOfTomorrow} = require('date-fns')
+const today = startOfToday();
+const tomorrow = startOfTomorrow();
 
-
-const handler = function(event, context, callback) {
-  var start = new Date();
-  start.setHours(0, 0, 1, 0);
-
-  const tomorrow = new Date(start);
-  tomorrow.setDate(tomorrow.getDate() + 1);
-
-  var thedate = function ISODateString(d) {
-    function pad(n) {
-      return n < 10 ? "0" + n : n;
-    }
-    return (
-      d.getUTCFullYear() +
-      "-" +
-      pad(d.getUTCMonth() + 1) +
-      "-" +
-      pad(d.getUTCDate()) +
-      "T" +
-      pad(d.getUTCHours()) +
-      ":" +
-      pad(d.getUTCMinutes()) +
-      ":" +
-      pad(d.getUTCSeconds()) +
-      "Z"
-    );
-  };
-
-  eventFullList = [];
-
-  calendar.events.list(
-    {
+const handler = function (event, context, callback) {
+  
+  
+  calendar.events.list({
       auth: oAuth2Client,
       calendarId: "8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com",
       singleEvents: true,
       showDeleted: false,
-      timeMin: (new Date()).toISOString(),
+      timeMin: today.toISOString(),
       maxResults: 10,
-      //timeMax: thedate(tomorrow)
+      timeMax: tomorrow.toISOString(),
       orderBy: "startTime"
     },
     (error, response) => {
@@ -75,4 +56,6 @@ const handler = function(event, context, callback) {
   );
 };
 
-module.exports = { handler };
+module.exports = {
+  handler
+};
