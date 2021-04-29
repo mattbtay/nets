b627f4012f9f35aa2b02a5c4f2736cf4a1145531
Matt Taylor <mattbtay@gmail.com>
2021-04-28 21:58:04 -0500

    clean up

--- netlify/functions/getEventList/getEventList.js
+++ netlify/functions/getEventList/getEventList.js
@@ -1,6 +1,10 @@
-const { google } = require("googleapis");
+const {
+  google
+} = require("googleapis");
 
-const { OAuth2 } = google.auth;
+const {
+  OAuth2
+} = google.auth;
 
 const oAuth2Client = new OAuth2(
   "526989770589-2n746t5hf2r30b7taodum5t6tfmh337k.apps.googleusercontent.com",
@@ -8,13 +12,15 @@ const oAuth2Client = new OAuth2(
 );
 
 oAuth2Client.setCredentials({
-  refresh_token:
-    "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
+  refresh_token: "1//04x5_ldyWu-hdCgYIARAAGAQSNwF-L9IrRG6enlngr3yV-Tmn6p3-OGz1TZa5qY8bwQ_ej-DiTrffnmtzp4qOpU0rQrxcUc0jNsE"
 });
 
-const calendar = google.calendar({ version: "v3", auth: oAuth2Client });
+const calendar = google.calendar({
+  version: "v3",
+  auth: oAuth2Client
+});
 
-const handler = function(event, context, callback) {
+const handler = function (event, context, callback) {
   var start = new Date();
   start.setHours(0, 0, 1, 0);
 
@@ -22,8 +28,7 @@ const handler = function(event, context, callback) {
   end.setDate(end.getDate() + 1);
   end.setHours(00, 00, 00, 000);
 
-  calendar.events.list(
-    {
+  calendar.events.list({
       auth: oAuth2Client,
       calendarId: "8pv1frn7h2ml914el8cu7gb9a0@group.calendar.google.com",
       singleEvents: true,
@@ -36,23 +41,17 @@ const handler = function(event, context, callback) {
         console.log(error);
         return;
       }
-      // callback(null, {
-      //   headers: {
-      //     "Access-Control-Allow-Origin": "*"
-      //   },
-      //   statusCode: 200,
-      //   body: JSON.stringify(response.data.items)
-      // });
+      callback(null, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        },
+        statusCode: 200,
+        body: JSON.stringify(response.data.items)
+      });
     }
   );
-
-  callback(null, {
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    statusCode: 200,
-    body: JSON.stringify([])
-  });
 };
 
-module.exports = { handler };
+module.exports = {
+  handler
+};
